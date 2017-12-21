3249ab0382151bdbe198ba03f315553d1852053d
Josh Crowder <josh.crowder@crowdstrike.com>
2017-12-21 16:47:32 +0000

    wip lights can now be controlled

--- lib/hotword/lea.py
+++ lib/hotword/lea.py
@@ -5,7 +5,7 @@ import paho.mqtt.client as mqtt
 
 interrupted = False
 
-MQTT_ADDRESS = '127.0.0.1'
+MQTT_ADDRESS = 'raspberrypi.local'
 MQTT_PORT = '1883'
 mqtt_client = mqtt.Client()
 
@@ -22,11 +22,11 @@ def interrupt_callback():
 def publish_detect():
   mqtt_client.connect(MQTT_ADDRESS, int(MQTT_PORT))
   mqtt_client.publish('lea/hotword/detected', payload="{\"siteId\":\"" + siteId + "\"}", qos=0)
-  mqtt_client.publish('hermes/hotword/default/detected', payload="{\"siteId\":\"" + siteId + "\"}", qos=0)
+  # Instead of trigger per room using siteId we can trigger with customData
+  # mqtt_client.publish('hermes/hotword/default/detected', payload="{\"siteId\":\"" + siteId + "\"}", qos=0)
 
   action = "{\"type\":\"action\",\"text\":null,\"canBeEnqueued\":false,\"intentFilter\":null}"
-  jsonString = "{\"siteId\":\"" + siteId + "\",\"init\":" + action + ""
-
+  jsonString = "{\"customData\":\"" + siteId + "\",\"init\":" + action + "}"
   mqtt_client.publish('hermes/dialogueManager/startSession', payload=jsonString, qos=0)
 
   return snowboydecoder.play_audio_file
--- lib/lights.rb
+++ lib/lights.rb
@@ -2,7 +2,7 @@ require "hue"
 require 'mqtt'
 
 hue_client = Hue::Client.new
-mqtt_client = MQTT::Client.connect(host: '127.0.0.1', port: 1883)
+mqtt_client = MQTT::Client.connect(host: 'raspberrypi.local', port: 1883)
 
 GROUP_NAME = 'Living room'
 BRIGHTNESS = 140
@@ -10,14 +10,20 @@ BRIGHTNESS = 140
 def set_group_lights(group, set_to_value)
   state_on = false
   group.lights.each do |light| 
-    light.brightness = BRIGHTNESS
-    if set_to_value == true
-      light.on!
-      state_on = true
-    else
-      state_on = false
-      light.off!
-    end
+    set_light(light, set_to_value)
+  end
+  state_on
+end
+
+def set_light(light, set_to_value)
+  state_on = false
+  light.brightness = BRIGHTNESS
+  if set_to_value == true
+    light.on!
+    state_on = true
+  else
+    state_on = false
+    light.off!
   end
   state_on
 end
@@ -27,16 +33,64 @@ group = hue_client.groups.find {|g| g.name === GROUP_NAME }
 state_on = set_group_lights(group, false)
 
 puts "Initial State: #{state_on}"
-# unless group
-#   throw "Group not found"
-# end
+unless group
+  throw "Group not found"
+end
 
-mqtt_client.subscribe('lea/hotword/detected')
+mqtt_client.subscribe('hermes/nlu/intentParsed')
 
 mqtt_client.get do |topic,message|
   response = JSON.parse(message)
-  group = hue_client.groups.find {|g| g.name === response["siteId"] }  
-  puts "Current State: #{state_on}"
-  state_on = set_group_lights(group, !state_on)
-  puts "New State: #{state_on}"
+  intent = response["intent"]["intentName"]
+  set_to_value = true
+  case intent
+  when 'josh:lightsTurnOnSet'
+    set_to_value = true
+  when 'josh:lightsTurnOff'
+    set_to_value = false
+  end
+
+  puts "Intent #{intent}"
+  puts "Set lights to: #{set_to_value}"
+
+  if response["slots"].empty?
+    puts 'handle all lights'
+    hue_client.lights.each do |light|
+      state_on = set_light(light, set_to_value)
+    end
+  else
+    room = response["slots"][0]["rawValue"]
+    puts "Handle room #{room}"
+    group = hue_client.groups.find {|g| g.name.downcase == room.downcase }
+    puts group.inspect
+    unless group.nil?
+      puts "Current State: #{state_on}"
+      state_on = set_group_lights(group, set_to_value)
+      puts "New State: #{state_on}"
+    end
+  end
 end
+
+
+# {
+#   "input": "bedroom lights on",
+#   "intent": {
+#     "intentName": "josh:lightsTurnOnSet",
+#     "probability": 0.8170398
+#   },
+#   "slots": [
+#     {
+#       "rawValue": "bedroom",
+#       "value": {
+#         "kind": "Custom",
+#         "value": "bedroom"
+#       },
+#       "range": {
+#         "start": 0,
+#         "end": 7
+#       },
+#       "entity": "house_room",
+#       "slotName": "house_room"
+#     }
+#   ]
+# }
